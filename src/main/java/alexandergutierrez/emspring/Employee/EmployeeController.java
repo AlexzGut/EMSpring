6e99a9df3b44e6db7f683104e5c721e3d079f586
Alexander Gutierrez <JohnAlexander.GutierrezGaviria@outlook.com>
2024-12-17 17:10:17 -0500

    Include methods to handle edition and deletion of employees

--- src/main/java/alexandergutierrez/emspring/Employee/EmployeeController.java
+++ src/main/java/alexandergutierrez/emspring/Employee/EmployeeController.java
@@ -3,16 +3,14 @@ package alexandergutierrez.emspring.Employee;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 @Slf4j
-@RequestMapping("employees")
+@RequestMapping("/employees")
 public class EmployeeController {
 
     @ModelAttribute("employees")
@@ -29,5 +27,17 @@ public class EmployeeController {
     @GetMapping
     public String employeeList(Model model) {
         return "employee-list";
+    }
+
+    @GetMapping("{id}/details")
+    public String employeeDetail(@PathVariable int id, Model model) {
+        model.addAttribute("employeeId", id);
+        return "employee-detail";
+    }
+
+    @DeleteMapping("{id}/delete")
+    public String employeeDelete(@PathVariable int id, Model model) {
+        model.addAttribute("employeeId", id);
+        return "redirect:/employees";
     }
 }
--- src/main/java/alexandergutierrez/emspring/Employee/WebConfig.java
+++ src/main/java/alexandergutierrez/emspring/Employee/WebConfig.java
@@ -1,2 +1,16 @@
-package alexandergutierrez.emspring.Employee;public class WebConfig {
+package alexandergutierrez.emspring.Employee;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration
+public class WebConfig {
+
+    // HiddenHttpMethodFilter allows simulation of HTTP methods
+    // like PUT and DELETE using a hidden input field in an HTML form
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 }
