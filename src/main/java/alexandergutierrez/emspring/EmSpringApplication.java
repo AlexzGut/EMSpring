4c773bc122f8848ad4efeb373576fe69739419a5
Alexander Gutierrez <JohnAlexander.GutierrezGaviria@outlook.com>
2024-12-17 15:39:38 -0500

    EMSpring application initialized

---
+++ src/main/java/alexandergutierrez/emspring/EmSpringApplication.java
@@ -0,0 +1,13 @@
+package alexandergutierrez.emspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmSpringApplication.class, args);
+    }
+
+}
