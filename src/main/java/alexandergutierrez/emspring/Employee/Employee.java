0f55889168b25ce3b33209aa264de2f069332a90
Alexander Gutierrez <JohnAlexander.GutierrezGaviria@outlook.com>
2024-12-17 17:09:19 -0500

    Add id field in Employee domain class

--- src/main/java/alexandergutierrez/emspring/Employee/Employee.java
+++ src/main/java/alexandergutierrez/emspring/Employee/Employee.java
@@ -1,15 +1,24 @@
 package alexandergutierrez.emspring.Employee;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class Employee {
 
+    private static int id_gen = 0;
+    private int id;
     private String name;
     private String email;
     private String address;
     private String jobTitle;
     private String department;
+
+    public Employee(String name, String email, String address, String jobTitle, String department) {
+        this.id = ++id_gen;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.jobTitle = jobTitle;
+        this.department = department;
+    }
 }
